b821a1cf93be2e44daab1e59b61ecccaa869039e
Carlo Sciolla <carlo.sciolla@gmail.com>
2016-03-28 16:45:23 +0200

    Better docs and pref pane

--- src/main/java/tk/skuro/idea/orgmode/highlight/OrgColorSettingsPage.java
+++ src/main/java/tk/skuro/idea/orgmode/highlight/OrgColorSettingsPage.java
@@ -47,6 +47,7 @@ public class OrgColorSettingsPage implements ColorSettingsPage {
         );
         addAttribute("org.editor.colorsettingspage.outline", OUTLINE_ATTR_KEY);
         addAttribute("org.editor.colorsettingspage.underline", UNDERLINE_ATTR_KEY);
+        addAttribute("org.editor.colorsettingspage.bold", BOLD_ATTR_KEY);
         addAttribute("org.editor.colorsettingspage.code", CODE_ATTR_KEY);
         addAttribute("org.editor.colorsettingspage.block.delimiter", BLOCK_DELIM_ATTR_KEY);
         addAttribute("org.editor.colorsettingspage.block.content", BLOCK_CONTENT_ATTR_KEY);
