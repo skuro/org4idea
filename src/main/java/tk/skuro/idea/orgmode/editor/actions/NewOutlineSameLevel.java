3548857032a7b8b250c21f74934a4013fd8470e7
Carlo Sciolla <carlo.sciolla@gmail.com>
2016-08-25 19:27:21 +0200

    Can now add a new outline and the caret is moved to the beginning of it, but the position in which it's created it's currently wrong in a few cases

--- src/main/java/tk/skuro/idea/orgmode/editor/actions/NewOutlineSameLevel.java
+++ src/main/java/tk/skuro/idea/orgmode/editor/actions/NewOutlineSameLevel.java
@@ -3,13 +3,15 @@ package tk.skuro.idea.orgmode.editor.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import tk.skuro.idea.orgmode.parser.OrgTokenTypes;
-import tk.skuro.idea.orgmode.psi.OrgOutlineBlock;
 import tk.skuro.idea.orgmode.psi.OrgPsiElementImpl;
 
 /**
@@ -22,35 +24,52 @@ public class NewOutlineSameLevel extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        final PsiElement element = e.getData(LangDataKeys.PSI_ELEMENT);
+        final PsiFile file = e.getData(LangDataKeys.PSI_FILE);
         final Editor editor = e.getData(LangDataKeys.EDITOR);
-
-        if (element != null && editor != null) {
-            addOutline(element, editor.getDocument());
+        if(editor!= null && file != null) {
+            final CaretModel caretModel = editor.getCaretModel();
+            final int offset = caretModel.getCurrentCaret().getOffset();
+            final PsiElement element = file.findElementAt(offset);
+            if(element != null) {
+                addOutline(element, editor, file);
+            }
         }
     }
 
-    private void addOutline(@NotNull PsiElement element, @NotNull Document document) {
+    private void addOutline(@NotNull PsiElement element, @NotNull Editor editor, @NotNull PsiFile file) {
+        final Document document = editor.getDocument();
+
         // first search if the caret is itself within an outline text
         PsiElement currentOutline = getParentOutlineOrSelf(element);
         if (currentOutline != null) {
-            document.insertString(currentOutline.getTextRange().getEndOffset(), createOutlineSameDepthAs(currentOutline));
+            addText(editor, file, currentOutline.getTextRange().getEndOffset(), createOutlineSameDepthAs(currentOutline));
         } else {
             // the text is not part of an outline, maybe we're in the body of one
             currentOutline = findPreviousOutline(element);
             if (currentOutline != null) {
-                document.insertString(currentOutline.getTextRange().getEndOffset(), createOutlineSameDepthAs(currentOutline));
+                addText(editor, file, currentOutline.getTextRange().getEndOffset(), createOutlineSameDepthAs(currentOutline));
             } else {
                 // there's no outline occurring before the caret, maybe there's one in the following text
                 currentOutline = findNextOutline(element);
                 if (currentOutline != null) {
-                    document.insertString(currentOutline.getTextRange().getStartOffset(), createOutlineSameDepthAs(currentOutline));
+                    addText(editor, file, currentOutline.getTextRange().getStartOffset(), createOutlineSameDepthAs(currentOutline));
                 } else {
                     // there's no outline in the full text
-                    document.insertString(document.getText().length(), "* ");
+                    addText(editor, file, document.getText().length(), "\n* ");
                 }
             }
         }
+    }
+
+    private void addText(@NotNull final Editor editor, @NotNull final PsiFile file, final int offset, @NotNull final String text) {
+        new WriteCommandAction.Simple<String>(editor.getProject(), file) {
+            @Override
+            protected void run() throws Throwable {
+                final Document document = editor.getDocument();
+                document.insertString(offset, text);
+                editor.getCaretModel().getCurrentCaret().moveToOffset(offset + text.length());
+            }
+        }.execute();
     }
 
     private PsiElement findNextOutline(PsiElement element) {
@@ -68,14 +87,16 @@ public class NewOutlineSameLevel extends AnAction {
     }
 
     private String createOutlineSameDepthAs(PsiElement currentOutline) {
-        return currentOutline.getText().split("\\s")[0] + " ";
+        return "\n" + currentOutline.getText().split("\\s")[0] + " ";
     }
 
     private PsiElement findPreviousOutline(PsiElement element) {
-        while (element.getParent() != null) {
-            element = element.getParent();
+        PsiElement candidate = element;
+        while (candidate != null &&
+                !candidate.getNode().getElementType().equals(OrgTokenTypes.OUTLINE_BLOCK)) {
+            candidate = candidate.getPrevSibling();
         }
-        return PsiTreeUtil.getPrevSiblingOfType(element, OrgOutlineBlock.class);
+        return candidate;
     }
 
     private PsiElement getParentOutlineOrSelf(@NotNull PsiElement element) {
