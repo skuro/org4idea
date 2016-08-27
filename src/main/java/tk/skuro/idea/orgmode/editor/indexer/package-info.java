dbb52d8497a5238c680603021fa14e10d6ec6792
Carlo Sciolla <carlo.sciolla@gmail.com>
2016-08-27 21:32:59 +0200

    removed cut'n'paste code from the markdown plugin

--- src/main/java/tk/skuro/idea/orgmode/editor/indexer/package-info.java
+++ src/main/java/tk/skuro/idea/orgmode/editor/indexer/package-info.java
@@ -2,11 +2,7 @@
  * {@code TODO} tracking for Orgmode.
  *
  * inspired from Markdown and Xml Idea Plugins
- *
- * FIXME: not working perfectly.
- * todoes items are detected but not highlighted, neither really indexer
- * @see: https://github.com/nicoulaj/idea-markdown/issues/211
- *
+
  * @author Adriean Khisbe <adriean.khisbe@live.fr>
  * @since 0.2
  */
